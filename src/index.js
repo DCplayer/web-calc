6f227d9b6f389c41e1ef00f12e4795d9435c7627
DCplayer <cas15151@uvg.edu.gt>
2018-11-16 19:53:49 -0600

    mejorando escritura de calculadora, aplicar calculos

--- src/index.js
+++ src/index.js
@@ -15,6 +15,49 @@ class Calculator extends React.Component{
 	}
 
 	insert(interaction){
+		if(this.state.number.length >= 9){
+			return
+		}
+		console.log(interaction)
+
+		switch (interaction){
+			case '1': 
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				if(this.state.number === '0'){
+					this.setState({
+						number: interaction
+					})
+				}
+				else{
+					let tempNumber = this.state.number + interaction
+					this.setState({
+						number: tempNumber
+					})
+				}
+			case '0':
+				if(this.state.number != '0' && interaction === '0'){
+					let value = this.state.number + '0'
+					this.setState({
+						number: value
+					})
+				}
+			case '.': 
+				if(!this.state.number.includes('.') && this.state.number.length < 8){
+					this.setState({
+						number: this.state.number + '.'
+					})
+				}
+
+		}
+		console.log(this.state.number)
+
 
 	}
 
@@ -70,6 +113,7 @@ class Calculator extends React.Component{
 
 	handleClick(){
 
+
 	}
 
 	render(){
@@ -80,23 +124,23 @@ class Calculator extends React.Component{
 					{this.state.number}
 					</div>
 					<button className = "button"> C </button>
-					<button className = "erase button"> 🠨 </button>
+					<button className = "erase button"> del. </button>
 					<button className = "button"> M </button>
 					<button className = "button"> / </button>
-					<button className = "button"> 7 </button>
-					<button className = "button"> 8 </button>
-					<button className = "button"> 9 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '7')}> 7 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '8')}> 8 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '9')}> 9 </button>
 					<button className = "button"> X </button>
-					<button className = "button"> 4 </button>
-					<button className = "button"> 5 </button>
-					<button className = "button"> 6 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '4')}> 4 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '5')}> 5 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '6')}> 6 </button>
 					<button className = "button"> - </button>
-					<button className = "button"> 1 </button>
-					<button className = "button"> 2 </button>
-					<button className = "button"> 3 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '1')}> 1 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '2')}> 2 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '3')}> 3 </button>
 					<button className = "button"> + </button>
-					<button className = "zero button"> 0 </button>
-					<button className = "button"> . </button>
+					<button className = "zero button" onClick = {this.insert.bind(this, '0')}> 0 </button>
+					<button className = "button" onClick = {this.insert.bind(this, '.')}> . </button>
 					<button className = "button"> = </button>
 				</div>
 				
