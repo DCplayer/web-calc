e594d59747a2e1957f2f1b24fc5d1899f57a2e56
CoffeeBrewer <cas15151@uvg.edu.gt>
2018-11-21 01:38:23 -0600

    .

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -6,6 +6,34 @@ import App from '../App.jsx'
 describe('My first collection of test', () => {
 	it('renders the add button',  () => {
 		const wrapper = shallow(<App/>)
-		expect(wrapper.find('button').text()).toEqual('Add')
+		expect(wrapper.find('button number').text()).toEqual('+')
+	})
+})
+
+describe('My first collection of test', () => {
+	it('renders the add button',  () => {
+		const wrapper = shallow(<App/>)
+		expect(wrapper.find('button').text()).toEqual('-')
+	})
+})
+
+describe('My first collection of test', () => {
+	it('renders the add button',  () => {
+		const wrapper = shallow(<App/>)
+		expect(wrapper.find('button').text()).toEqual('X')
+	})
+})
+
+describe('My first collection of test', () => {
+	it('renders the add button',  () => {
+		const wrapper = shallow(<App/>)
+		expect(wrapper.find('button').text()).toEqual('/')
+	})
+})
+
+describe('My first collection of test', () => {
+	it('renders the add button',  () => {
+		const wrapper = shallow(<App/>)
+		expect(wrapper.find('button').text()).toEqual('.')
 	})
 })
